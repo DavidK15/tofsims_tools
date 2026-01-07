68111db52fa7a6c71f7859a3a7f879f4366d808b
DavidK15 <dkoepplinger3@gmail.com>
2026-01-07 12:54:39 +0100

    Update functions.py

--- tofsims_tools/functions.py
+++ tofsims_tools/functions.py
@@ -1,11 +1,38 @@
 import numpy as np
 
 def normalize_spectrum(intensity):
+	"""
+	Normalisiert ein Intensitätsarray auf 1."
+	"""
 	intensity = np.asarray(intensity)
-	return intensity / np.sum(intensity)
+	total = np.sum(intensity)
+	if total ==0:
+		return intensity
+	return intensity / total
 
 def interate_peak(mz, intensity, center, width):
+	"""
+	Integriert die Intensität um einen m/z-Wert (peak center) +- width/2.
+	"""
 	mz = np.asarray(mz)
 	intensity = np.asarray(intensity)
 	mask = (mz >= center - width/2) & (mz <= center + width/2)
 	return np.sum(intensity[mask])
+
+#Imaging TOF-SIMS
+
+def sum_image_pixels(image_data):
+	"""
+	Summiert die Intensität aller Pixel eines 2D-Arrays.
+	"""
+	return np.sum(image_data)
+
+def normalize_image(image_data):
+	"""
+	Normalisiert eine 2D-Array auf 1.
+	"""
+	image_data = np.asarray(image_data, dtype=float)
+    total = np.sum(image_data)
+    if total == 0:
+        return image_data
+    return image_data / total
